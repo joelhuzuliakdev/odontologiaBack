feabb3331b4cf5a65f4af9a3db526174bf8bca53
Paula-Osella <paulaosella19@gmail.com>
2026-03-18 22:19:11 -0300

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 import express from "express"
 import cors from "cors"
 import appointmentRoutes from "./routes/appointment.routes.js"
+import dotenv from "dotenv"
 
 
 const app = express()
-
+const PORT = process.env.PORT || 3000
+dotenv.config()
 app.use(cors())
 app.use(express.json())
 
 app.use("/api", appointmentRoutes)
 
-app.listen(3000, () => {
-console.log("Servidor corriendo en http://localhost:3000")
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`)
 })
